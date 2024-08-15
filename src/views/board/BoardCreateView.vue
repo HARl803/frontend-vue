c3d8df81c0ea53829ead7f7c7e8df2200b8e192b
chaeniyoon <yoonchaeyeon1101@gmail.com>
2024-08-16 01:34:48 +0900

    community

--- src/views/board/BoardCreateView.vue
+++ src/views/board/BoardCreateView.vue
@@ -9,7 +9,6 @@
         적극적인 소통을 기대합니다!
       </p>
     </div>
-    {{ $route.params }}
 
     <!-- 2. 게시글 작성 폼 영역 -->
     <!-- 2-1. 게시글 카테고리 선택 -->
@@ -88,7 +87,7 @@ export default {
     const router = useRouter();
     const route = useRoute();
     const postDetail = ref(null);
-    const API_URL = "/api/v1/community";
+    const API_URL = "/api/v1";
     const isEditMode = ref(false);
 
     // 게시글 유형
@@ -103,43 +102,83 @@ export default {
     };
 
     onMounted(() => {
-      console.log(route);
+
+      console.log(route.query);
       
-      isEditMode.value = true;
-      postTitle.value = route.params.postData.postTitle;
-      postContent.value = route.params.postData.postContent;
-      postTypeId.value = route.params.postData.postTypeId;
-  });
+      // 수정 모드인지 확인
+      if (route.query.mode === 'edit') {
+        isEditMode.value = true;
+
+        if (route.query.postData) {
+          try {
+            const postData = JSON.parse(route.query.postData);
+            console.log(postData);
+            postTitle.value = postData.postTitle;
+            postContent.value = postData.postContent;
+            postTypeId.value = postData.postTypeId;
+          } catch (e) {
+            console.error('Error parsing postData:', e);
+          }
+        } else {
+          console.error('postData is undefined');
+        }
+        // const postData = JSON.parse(route.params.postData);
+
+        // console.log(postData)
+        // postTitle.value = postData.postTitle;
+        // postContent.value = postData.postContent;
+        // postTypeId.value = postData.postTypeId;
+        // 이미지 파일은 수정하지 않으면 그대로 유지
+      } 
+    });
 
     // 게시글 저장 (생성 또는 수정)
     const savePost = async () => {
-      console.log(router.params.postId)
-      const formData = new FormData();
-      formData.append("postId", route.params.postId)
-      formData.append("postTypeId", postTypeId.value);
-      formData.append("postTitle", postTitle.value);
-      formData.append("postContent", postContent.value);
+      // const formData = new FormData();
+      // formData.append("postTypeId", postTypeId.value);
+      // formData.append("postTitle", postTitle.value);
+      // formData.append("postContent", postContent.value);
 
-      if (postImageFile.value) {
-        formData.append("postImageFile", postImageFile.value);
+      // console.log(postTypeId.value)
+      // console.log(postTitle.value)
+      // console.log(formData)
+
+      // if (postImageFile.value) {
+      //   formData.append("postImageFile", postImageFile.value);
+      // }
+
+      const data = {
+        postTypeId:postTypeId.value,
+        postTitle: postTitle.value,
+        postCotent: postContent.value,
+        
       }
 
+      console.log(data)
+      
       try {
         if (isEditMode.value) {
           // 게시글 수정
-          await axios.patch(`${API_URL}/post`, formData, {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          });
+          
+          await axios.patch(`${API_URL}/post`, data
+          // {
+          //   headers: {
+          //     "Content-Type": "multipart/form-data",
+          //   },
+          // }
+        );
           console.log("게시글 수정 성공!");
         } else {
+
+          console.log(data)
           // 새 게시글 생성
-          await axios.post(`${API_URL}/post`, formData, {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          });
+          await axios.post(`${API_URL}/post`, data
+          // , {
+          //   headers: {
+          //     "Content-Type": "multipart/form-data",
+          //   },
+          // }
+        );
           console.log("게시글 생성 성공!");
         }
         router.push({ name: "community" });
